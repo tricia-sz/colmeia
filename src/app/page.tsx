fac06502b21972d5acedadbcbdf9b0e2e99ef771
Patrícia Souza <patricia.sz.br@gmail.com>
2023-11-24 12:22:39 -0300

    Tipografy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,26 +14,26 @@ export default function Home() {
       <Patricia />
     </div>
     <div className="flex container justify-center items-center">
-     <section className=" w-full container gap-2 py-16 px-4 cursor-default hover:scale-110 transition-transform">
+     <section className=" w-full container gap-2 py-16 px-4 ">
         <div className=" w-full flex">
          <h1 className="text-left font-bold text-3xl  text-slate-700">Sobre a Colmeia</h1>
         </div>
           <div>
-            <div className="text-left  text-3xl mt-8 text-slate-500 align-baseline">
-              <p className="font-light">
+            <div className="text-left  text-2xl mt-8 text-slate-500 align-baseline leading-relaxed">
+              <p className="font-light cursor-default hover:scale-110 transition-transform">
               A <Link href="https://colmeia.me/" className="text-emerald-500 font-bold">Colmeia</Link> é a tecnologia líder do mercado em comunicação digital. A plataforma possui atendimento automatizado, atendimento humano, marketing e analytics. Recebemos milhares de conexões por segundo, trabalhamos com tecnologia de ponta, clientes importantes. 
               </p>
             </div>
           </div>
       </section>
      <MdHive size={150} className="text-slate-400"/>
-      <section className=" w-full container gap-2 py-16 px-4 cursor-default hover:scale-110 transition-transform">
+      <section className=" w-full container gap-2 py-16 px-4">
         <div className=" w-full flex ml-4">
          <h1 className="text-left font-bold text-3xl  text-slate-700">Sobre a Vaga: <span className=" text-emerald-500 font-medium">Desenvolvedor de APIs – Javascript</span></h1>
         </div>
           <div>
-            <div className="text-left  text-3xl mt-8 text-slate-500 align-baseline">
-              <p className="font-light ml-8">
+            <div className="text-left  text-2xl mt-8 text-slate-500 align-baseline leading-relaxed">
+              <p className="font-light ml-8 cursor-default hover:scale-110 transition-transform">
                 PJ, híbrida (2x ou 3x por semana). O escritório é ao lado do metrô Faria Lima.
                 Quando fazemos uma jornada de atendimento, frequentemente temos que nos conectar a APIs existentes 
                 de nossos clientes. A plataforma contém uma IDE completa em Typescript e recursos avançados para fazer isso tipo de conexão.
@@ -49,12 +49,12 @@ export default function Home() {
     <section className="text-3xl bg-slate-400 py-4 px-4 justify-center items-center container rounded-xl mb-8">
       <div className="">
         <p className="text-emerald-900 text-3xl items-center flex  mb-2 mt-4">Questão 1:</p>
-        <p className="text-emerald-900 text-2xl font">
+        <p className="text-emerald-900 text-2xl font leading-relaxed font-light">
           O parâmetro numbers é um Array. Sua tarefa é percorrer esse array e:
           se for divizivel por 5, print "buzz". Se for divisivel por 3 e 5, print "fizzBuzz"
         </p>
         <span className="text-emerald-700 pl-4 text-lg">
-          Código resolvido:
+          Solução:
           <div>
             <Image alt="codigo resolvido" src={fizzbuzz}  className="object-cover rounded-xl mb-4 cursor-default hover:scale-110 transition-transform" >
 
@@ -67,7 +67,7 @@ export default function Home() {
     <section className="text-3xl bg-slate-400 py-4 px-4 justify-center items-center container rounded-xl mb-4 ">
       <div className="">
         <p className="text-emerald-900 text-3xl items-center flex  mb-2 mt-4">Questão 2:</p>
-        <p className="text-emerald-900 text-2xl font">
+        <p className="text-emerald-900 text-2xl font leading-relaxed font-light">
           Temos uma variável book. Ela é uma string e contem apenas letras minúsculas sem acento e espaços. <br />
            Sua tarefa é percorrer a variável book e contar a quantidade de ocorrências de cada letra E o número de palavras.<br />
            EXEMPLO DE OUTPUT: 12333 palavras;  a: 1032, b:129, c: 1111 etc.<br />
@@ -75,7 +75,7 @@ export default function Home() {
            const book"este e o conteudo apenas com palavras minusculas e espacos";
         </p>
         <span className="text-emerald-700 pl-4 text-lg">
-          Código resolvido:
+          Solução:
           <div>
             <Image alt="codigo resolvido" src={book}  className="object-cover rounded-xl mb-4 cursor-default hover:scale-110 transition-transform" >
 
--- src/components/Patricia.tsx
+++ src/components/Patricia.tsx
@@ -9,20 +9,20 @@ function Patricia() {
   return (
     
    <> 
-      <section className=" w-full container gap-2 cursor-default hover:scale-110 transition-transform mx-8">
+      <section className=" w-full container gap-2 cursor-default  mx-8">
         <div className=" w-full flex">
         <BiHive size={30} className="text-slate-700"/><h1 className="text-left font-bold text-3xl  text-slate-700">Candidato(a): <span className=" text-emerald-500 font-medium">Patrícia Souza</span> </h1>
         </div>
         <div>  
-        <div className="text-left  text-3xl mt-4 text-slate-500 align-baseline pl-8 font-light">
-          <Link href={""} className="flex  gap-2 hover hover:text-emerald-500 cursor-default hover:scale-110 transition-transform">
+        <div className="text-left  text-2xl mt-4 text-slate-500 align-baseline pl-8 font-light">
+          <Link href={""} className="flex  gap-2 hover hover:text-emerald-500 ">
             <MdLocationPin />Local: São Paulo - SP
           </Link>
-          <Link  href="https://tricia-sz.netlify.app/" className="flex gap-2 hover hover:text-emerald-500 mt-4 cursor-default hover:scale-110 transition-transform">
-            <FaChrome /> Portifólio do candidato
+          <Link  href="https://tricia-sz.netlify.app/" className="flex gap-2 hover hover:text-emerald-500 mt-4 ">
+            <FaChrome /> Portifólio do candidato(a)
           </Link>
-          <Link href="https://github.com/tricia-sz" className="flex gap-2 hover hover:text-emerald-500 mt-4 cursor-default hover:scale-110 transition-transform">
-            <FaGithub />tricia-sz
+          <Link href="https://github.com/tricia-sz" className="flex gap-2 hover hover:text-emerald-500 mt-4 ">
+            <FaGithub />Perfil do Github 
           </Link>
         </div>
       </div>
